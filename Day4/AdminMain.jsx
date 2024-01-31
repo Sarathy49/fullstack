c97972220d7d5c1bcb1c6c82c4015eb065d543be
Sarathy49 <119049025+Sarathy49@users.noreply.github.com>
2024-01-31 14:30:59 +0530

    Add files via upload

---
+++ Day4/AdminMain.jsx
@@ -0,0 +1,94 @@
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import {
+  MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBCard,
+  MDBCardText,
+  MDBCardBody,
+
+} from 'mdb-react-ui-kit';
+
+import CustomSidenav from '../components/CustomSidenav';
+import LineChart from '../components/LineChart';
+
+  
+  
+  
+  const AdminMain = () => {
+    return (
+      <>    
+  
+            <div style={{display:"flex"}}>
+              <CustomSidenav/>
+              <div>
+
+             
+              <MDBContainer className="py">
+              <MDBRow>
+                
+                  <MDBCol style={{marginLeft:"auto",marginRight:"0%",padding:"20px"}} lg="8">
+                      <MDBCard style={{}} className="mb-4">
+                          <MDBCardBody>
+                              <MDBRow>
+                                  <MDBCol sm="3">
+                                      <MDBCardText>Full Name</MDBCardText>
+                                  </MDBCol>
+                                  <MDBCol sm="9">
+                                      <MDBCardText className="text-muted">Sarathy</MDBCardText>
+                                  </MDBCol>
+                              </MDBRow>
+                              <hr />
+                              <MDBRow>
+                                  <MDBCol sm="3">
+                                      <MDBCardText>Email</MDBCardText>
+                                  </MDBCol>
+                                  <MDBCol sm="9">
+                                      <MDBCardText className="text-muted">sarathy934@gmail.com</MDBCardText>
+                                  </MDBCol>
+                              </MDBRow>
+                              <hr />
+                              <MDBRow>
+                                  <MDBCol sm="3">
+                                      <MDBCardText>Phone</MDBCardText>
+                                  </MDBCol>
+                                  <MDBCol sm="9">
+                                      <MDBCardText className="text-muted">9345723462</MDBCardText>
+                                  </MDBCol>
+                              </MDBRow>
+                              <hr />
+                              <MDBRow>
+                                  <MDBCol sm="3">
+                                      <MDBCardText>Role</MDBCardText>
+                                  </MDBCol>
+                                  <MDBCol sm="9">
+                                      <MDBCardText className="text-muted">Admin</MDBCardText>
+                                  </MDBCol>
+                              </MDBRow>
+                              <hr />
+                             
+                          </MDBCardBody>
+                      </MDBCard>
+
+
+                  </MDBCol>
+              </MDBRow>
+          </MDBContainer>
+          </div>
+      
+            
+            <div style={{marginTop:"25%",marginLeft:"-35%"}} >
+                <br>
+                </br>
+                <br></br>
+                <br></br>
+               
+                <LineChart/>
+                </div>      
+        </div> 
+      </>
+    )
+  }
+  
+  export default AdminMain
+  
