404d61d8a22ab93c2cb85e295e478a5beead46f6
Sarathy49 <119049025+Sarathy49@users.noreply.github.com>
2024-01-31 14:24:34 +0530

    Add files via upload

---
+++ Day2/CustomNavebar.jsx
@@ -0,0 +1,29 @@
+import { Nav, Navbar } from "rsuite"
+import HomeIcon from '@rsuite/icons/legacy/Home';
+import { Link } from "react-router-dom";
+
+const CustomNavebar = () => {
+  return (
+    <Navbar>
+    <Navbar.Brand href="#">EVENTO</Navbar.Brand>
+    <Nav pullRight>
+      <Nav.Item icon={<HomeIcon />}><Link to={'/home'}>Home</Link></Nav.Item>
+      <Nav.Item><Link to={'/about'}>About</Link></Nav.Item>
+      <Nav.Item><Link to={'/contact'}>Contact</Link></Nav.Item>
+      <Nav.Menu title="Events">
+
+      <Nav.Item><Link to="/allevent">All Events</Link></Nav.Item>
+      <Nav.Item><Link to="/find-events">Find Events</Link></Nav.Item>
+      </Nav.Menu>
+      <Nav.Menu title="Profile">
+        <Nav.Item><Link to="/profile">My Profile</Link></Nav.Item>
+        <Nav.Item>Login</Nav.Item>
+      </Nav.Menu>
+    </Nav>
+    
+  </Navbar>
+    
+  )
+}
+
+export default CustomNavebar
